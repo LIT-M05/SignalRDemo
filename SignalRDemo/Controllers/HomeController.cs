5717e1246b84a7d1a804beecede4cee06fe0c5f5
Avrumi Friedman <alex+6@lakewoodprogramming.com>
2018-06-01 18:48:54 -0400

    Initial commit

---
+++ SignalRDemo/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public void NewMessage(string message)
+        {
+            var hub = GlobalHost.ConnectionManager.GetHubContext<SimpleHub>();
+            hub.Clients.All.newMessageReceived(message);
+        }
+
+    }
+}
